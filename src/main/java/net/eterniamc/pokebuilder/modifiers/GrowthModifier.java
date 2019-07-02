6dbfc83a7b62a35d75b53962d6090bf80ae4a3f3
d4julian <julianubico@icloud.com>
2019-07-02 05:03:02 -0400

    code cleanup
    Took 57 minutes

--- src/main/java/net/eterniamc/pokebuilder/modifiers/GrowthModifier.java
+++ src/main/java/net/eterniamc/pokebuilder/modifiers/GrowthModifier.java
@@ -10,8 +10,8 @@ import com.pixelmonmod.pixelmon.config.PixelmonItemsBadges;
 import com.pixelmonmod.pixelmon.config.PixelmonItemsHeld;
 import com.pixelmonmod.pixelmon.enums.EnumGrowth;
 import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import net.eterniamc.pokebuilder.Configuration.Config;
 import net.eterniamc.pokebuilder.ModifierData;
-import net.eterniamc.pokebuilder.PokeBuilder;
 import net.eterniamc.pokebuilder.Utils;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
@@ -34,23 +34,21 @@ public class GrowthModifier implements Modifier {
     public boolean run(ModifierData data) {
         Pokemon pixelmon = data.getPokemon();
         Player player = data.getPlayer();
-        StateContainer container = data.getGui() == null ? new StateContainer() : data.getGui();
+        StateContainer container = data.getGui();
         if (container.hasState("growth"))
             container.removeState("growth");
-        Page.PageBuilder builder = Page.builder()
+        Page.PageBuilder natureModifier = Page.builder()
                 .setAutoPaging(true)
                 .setTitle(Text.of("Growth Modifier"))
                 .setParent("editor")
                 .setEmptyStack(Utils.empty());
-        if (data.getGui() == null)
-            builder.setParent(null);
         List<EnumGrowth> growths = new ArrayList<>(Arrays.asList(EnumGrowth.values()));
         growths.sort((growth1, growth2) -> growth1.scaleValue - growth2.scaleValue > 0 ? 1 : growth1.scaleValue - growth2.scaleValue < 0 ? -1 : 0);
         for (EnumGrowth growth : growths)
-            builder.addElement(new ActionableElement(
+            natureModifier.addElement(new ActionableElement(
                             new RunnableAction(container, ActionType.CLOSE, "", context -> {
                                 pixelmon.setGrowth(growth);
-                                Utils.withdraw(player, PokeBuilder.config.growthModifierCost * (Arrays.stream(EnumSpecies.LEGENDARY_ENUMS).anyMatch(p -> pixelmon.getSpecies() == p) || pixelmon.getSpecies() == EnumSpecies.Ditto ? PokeBuilder.config.legendaryOrDittoMultiplier : 1));
+                                Utils.withdraw(player, Config.growthModifierCost * (Arrays.stream(EnumSpecies.LEGENDARY_ENUMS).anyMatch(p -> pixelmon.getSpecies() == p) || pixelmon.getSpecies() == EnumSpecies.Ditto ? Config.legendaryOrDittoMultiplier : 1));
 
                             }),
                             ItemStack.builder()
@@ -60,7 +58,7 @@ public class GrowthModifier implements Modifier {
                                     .build()
                     )
             );
-        container.addState(builder.build("growth"));
+        container.addState(natureModifier.build("growth"));
         container.openState(player, "growth");
         return false;
     }
@@ -72,6 +70,6 @@ public class GrowthModifier implements Modifier {
 
     @Override
     public double getCost(Pokemon pokemon) {
-        return PokeBuilder.config.growthModifierCost * getMultiplier(pokemon);
+        return Config.growthModifierCost * getMultiplier(pokemon);
     }
 }
--- src/main/java/net/eterniamc/pokebuilder/modifiers/PokeballModifier.java
+++ src/main/java/net/eterniamc/pokebuilder/modifiers/PokeballModifier.java
@@ -9,8 +9,8 @@ import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.config.PixelmonItemsPokeballs;
 import com.pixelmonmod.pixelmon.enums.EnumSpecies;
 import com.pixelmonmod.pixelmon.enums.items.EnumPokeballs;
+import net.eterniamc.pokebuilder.Configuration.Config;
 import net.eterniamc.pokebuilder.ModifierData;
-import net.eterniamc.pokebuilder.PokeBuilder;
 import net.eterniamc.pokebuilder.Utils;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
@@ -31,21 +31,19 @@ public class PokeballModifier implements Modifier {
     public boolean run(ModifierData data) {
         Pokemon pixelmon = data.getPokemon();
         Player player = data.getPlayer();
-        StateContainer container = data.getGui() == null ? new StateContainer() : data.getGui();
+        StateContainer container = data.getGui();
         if (container.hasState("pokeball"))
             container.removeState("pokeball");
-        Page.PageBuilder builder = Page.builder()
+        Page.PageBuilder natureModifier = Page.builder()
                 .setAutoPaging(true)
                 .setTitle(Text.of("Pokeball Modifier"))
                 .setParent("editor")
                 .setEmptyStack(Utils.empty());
-        if (data.getGui() == null)
-            builder.setParent(null);
         for (EnumPokeballs ball : EnumPokeballs.values())
-            builder.addElement(new ActionableElement(
+            natureModifier.addElement(new ActionableElement(
                             new RunnableAction(container, ActionType.CLOSE, "", context -> {
                                 pixelmon.setCaughtBall(ball);
-                                Utils.withdraw(player, PokeBuilder.config.pokeballModifierCost * (Arrays.stream(EnumSpecies.LEGENDARY_ENUMS).anyMatch(p -> pixelmon.getSpecies() == p) || pixelmon.getSpecies() == EnumSpecies.Ditto ? PokeBuilder.config.legendaryOrDittoMultiplier : 1));
+                                Utils.withdraw(player, Config.pokeballModifierCost * (Arrays.stream(EnumSpecies.LEGENDARY_ENUMS).anyMatch(p -> pixelmon.getSpecies() == p) || pixelmon.getSpecies() == EnumSpecies.Ditto ? Config.legendaryOrDittoMultiplier : 1));
 
                             }),
                             ItemStack.builder()
@@ -55,7 +53,7 @@ public class PokeballModifier implements Modifier {
                                     .build()
                     )
             );
-        container.addState(builder.build("pokeball"));
+        container.addState(natureModifier.build("pokeball"));
         container.openState(player, "pokeball");
         return false;
     }
@@ -67,6 +65,6 @@ public class PokeballModifier implements Modifier {
 
     @Override
     public double getCost(Pokemon pokemon) {
-        return PokeBuilder.config.pokeballModifierCost * getMultiplier(pokemon);
+        return Config.pokeballModifierCost * getMultiplier(pokemon);
     }
 }
